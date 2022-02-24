be57d198167165a61ad0120c6c53c6bc312f295c
wutthichai.s <qqwut@hotmail.com>
2022-02-24 20:37:49 +0700

    test

--- server/routes/draft.routes.js
+++ server/routes/draft.routes.js
@@ -1,18 +1,17 @@
-// module.exports = function () {
-//   const config = require('../config/config').get(process.env.NODE_ENV).prefix_api;
-//   var draftCtrl = require('../controller/draft-ctrl');
-//   router.get('/draft-list', draftCtrl.draftList);
-//   app.delete('/draft-delete', draftCtrl.draftDelete);
-//   // app.delete(config + '/draft/delete-draft', draftCtrl.deleteDraft);
-// };
-
+import rateLimit from 'express-rate-limit'
+const createAccountLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 5, // Limit each IP to 5 create account requests per `window` (here, per hour)
+    message:
+        'Too many accounts created from this IP, please try again after an hour',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 // const config = require('../config/config').get(process.env.NODE_ENV).prefix_api;
 const express = require('express');
 const draftCtrl = require('../controller/draft-ctrl');
 var router = express.Router();
-router.route('/draft-list').get((req, res) => {
-    draftCtrl.draftList(req, res)
-})
+router.route('/draft-list').get(draftCtrl.draftList, createAccountLimiter)
 router.route('/draft-detail').get(draftCtrl.draftDetail);
 router.route('/draft-create').post(draftCtrl.draftCreate);
 router.route('/draft-save/:draftId').put(draftCtrl.draftSave);
--- server/routes/locationAddress.routes.js
+++ server/routes/locationAddress.routes.js
@@ -7,11 +7,20 @@ const apiLimiter = rateLimit({
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
+const createAccountLimiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 1 hour
+	max: 5, // Limit each IP to 5 create account requests per `window` (here, per hour)
+	message:
+		'Too many accounts created from this IP, please try again after an hour',
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 // const config = require('../config/config').get(process.env.NODE_ENV).prefix_api;
 const express = require('express');
 const locationAddress = require('../controller/locationAddress-ctrl');
 var router = express.Router();
 
-router.route('/get-address', apiLimiter).get(locationAddress.getAddress);
+router.route('/get-address', apiLimiter).get(createAccountLimiter,locationAddress.getAddress);
 // router.route('/draft-delete').delete(draftCtrl.draftDelete);
 module.exports = router;
