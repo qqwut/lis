6c58973a0d07e99047497da1def7fdde2c885835
wutthichai.s <qqwut@hotmail.com>
2022-02-24 20:11:19 +0700

    fix

--- server/routes/locationAddress.routes.js
+++ server/routes/locationAddress.routes.js
@@ -1,16 +1,3 @@
-var express = require('express');
-var app = express();
-
-// set up rate limiter: maximum of five requests per minute
-var RateLimit = require('express-rate-limit');
-var limiter = new RateLimit({
-    windowMs: 1 * 60 * 1000, // 1 minute
-    max: 5
-});
-
-// apply rate limiter to all requests
-app.use(limiter);
-const express = require('express');
 const locationAddress = require('../controller/locationAddress-ctrl');
 var router = express.Router();
 
