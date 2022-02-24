100af25e4cb3eb5f58be3180b27e1e72fe9786dc
wutthichai.s <qqwut@hotmail.com>
2022-02-24 20:35:59 +0700

    fix

--- server/routes/locationAddress.routes.js
+++ server/routes/locationAddress.routes.js
@@ -1,20 +1,17 @@
-// module.exports = function(app) {
-//   var locationAddress = require('../controller/locationAddress-ctrl');
-//   app.get('/api/location-address/get-country', locationAddress.getCountry);
-//   app.get('/api/location-address/get-province', locationAddress.getProvince);
-//   app.get('/api/location-address/get-address', locationAddress.getAddress);
-// };
+import rateLimit from 'express-rate-limit'
 
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 // const config = require('../config/config').get(process.env.NODE_ENV).prefix_api;
 const express = require('express');
 const locationAddress = require('../controller/locationAddress-ctrl');
 var router = express.Router();
 
-const rateLimiter = (req, res, next) => {
-    // rate limiter logic here
-}
-
-router.route('/get-address').get(locationAddress.getAddress, rateLimiter);
+router.route('/get-address', apiLimiter).get(locationAddress.getAddress);
 // router.route('/draft-delete').delete(draftCtrl.draftDelete);
 module.exports = router;
