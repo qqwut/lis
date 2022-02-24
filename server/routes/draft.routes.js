26c47fbf735bed6c54872b650c992bdfcec793e1
wutthichai.s <qqwut@hotmail.com>
2022-02-24 20:20:42 +0700

    fix

--- server/routes/draft.routes.js
+++ server/routes/draft.routes.js
@@ -10,7 +10,9 @@
 const express = require('express');
 const draftCtrl = require('../controller/draft-ctrl');
 var router = express.Router();
-router.route('/draft-list').get(draftCtrl.draftList);
+router.route('/draft-list').get((req, res) => {
+    draftCtrl.draftList(req, res)
+})
 router.route('/draft-detail').get(draftCtrl.draftDetail);
 router.route('/draft-create').post(draftCtrl.draftCreate);
 router.route('/draft-save/:draftId').put(draftCtrl.draftSave);
--- server/routes/lov.routes.js
+++ server/routes/lov.routes.js
@@ -1,7 +1,8 @@
 // const config = require('../config/config').get(process.env.NODE_ENV).prefix_api;
 const express = require('express');
+const lovCtrl = require('../controller/lov-ctrl');
 var router = express.Router();
-router.route('/get-lov').get(require('../controller/lov-ctrl').getLov);
-router.route('/get-province').get(require('../controller/lov-ctrl').getProvince);
+router.route('/get-lov').get(lovCtrl.getLov);
+router.route('/get-province').get(lovCtrl.getProvince);
 
 module.exports = router;
