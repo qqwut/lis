5e6f56294a201dd92c5d71cc405b36a10f2e6640
wutthichai.s <qqwut@hotmail.com>
2022-02-24 20:25:30 +0700

    fix

--- server/routes/locationAddress.routes.js
+++ server/routes/locationAddress.routes.js
@@ -1,21 +1,20 @@
-var express = require('express');
-var app = express();
+// module.exports = function(app) {
+//   var locationAddress = require('../controller/locationAddress-ctrl');
+//   app.get('/api/location-address/get-country', locationAddress.getCountry);
+//   app.get('/api/location-address/get-province', locationAddress.getProvince);
+//   app.get('/api/location-address/get-address', locationAddress.getAddress);
+// };
 
-// set up rate limiter: maximum of five requests per minute
-var RateLimit = require('express-rate-limit');
-var limiter = new RateLimit({
-    windowMs: 1 * 60 * 1000, // 1 minute
-    max: 5
-});
 
-// apply rate limiter to all requests
-app.use(limiter);
+// const config = require('../config/config').get(process.env.NODE_ENV).prefix_api;
 const express = require('express');
 const locationAddress = require('../controller/locationAddress-ctrl');
 var router = express.Router();
 
-router.route('/get-address').get(function (req, res) {
-    locationAddress.getAddress(req, res)
-});
+const rateLimiter = (req, res, next) => {
+    // rate limiter logic here
+}
+
+router.route('/get-address', rateLimiter).get(locationAddress.getAddress);
 // router.route('/draft-delete').delete(draftCtrl.draftDelete);
 module.exports = router;
